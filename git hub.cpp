c37386d39cc453ef7d237d220f82b11af404ef85
Your Name <you@example.com>
2024-08-10 20:57:52 +0300

    stroki7

--- git hub.cpp
+++ git hub.cpp
@@ -14,11 +14,15 @@ int main()
         getline(cin, str1);
         cout << str1;
         cout << str1[0] << " " << str1[3];
-        cout << "Lengs " << str1 << " =" << str.size() << endl;
-        for (int i = 0; i < str.size(); ++i) {
-            cout << i << "\t" << str[i];
+        cout << "Lengs " << str1 << " =" << str1.size() << endl;
+        for (int i = 0; i < str1.size(); ++i) {
+            cout << i << "\t" << str1[i];
         }
-
+        string str2 = "QWE", str3 = "QWE";
+        if (str2 == str3) {
+            cout << str2;
+        }
+        
 }
 
 
